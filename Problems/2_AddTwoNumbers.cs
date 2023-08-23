2b3252df3811b6563dab0bfc33bcdf37b914c7c4
Pavel Andreevich <paandu.da@gmail.com>
2023-08-23 14:22:50 +0900

    86% Runtime, 48% Memory

---
+++ Problems/2_AddTwoNumbers.cs
@@ -0,0 +1,55 @@
+﻿namespace Solutions.Problems
+{
+    internal class _2_AddTwoNumbers
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode current = new(0);
+            ListNode Head = new(0, current);
+
+            int next = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                int x = l1?.val ?? 0;
+                int y = l2?.val ?? 0;
+
+                current.val = x + y + next;
+
+                if (current.val >= 10)
+                {
+                    current.val = current.val - 10;
+                    next = 1;
+                }
+                else
+                {
+                    next = 0;
+                }
+
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+
+                if (l1 != null || l2 != null || next > 0)
+                {
+                    current.next = new(0 + next);
+                    current = current.next;
+                }
+
+            }
+
+
+            return Head.next;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
