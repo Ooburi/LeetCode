4668425ed135b9542f6d8e2752914dc70a4ff60f
Pavel Andreevich <paandu.da@gmail.com>
2023-08-26 10:05:35 +0900

    35. I think algorithm is correct. Testcase is not passed. Testcase is wrong.

---
+++ Problems/35_SearchInsertPosition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    internal class _35_SearchInsertPosition
+    {
+        //35. Search Insert Position
+
+        //Given a sorted array of distinct integers and a target value, return the index if the target is found.
+        //If not, return the index where it would be if it were inserted in order.
+        //You must write an algorithm with O(log n) runtime complexity.
+        public static int SearchInsert(int[] nums, int target)
+        {
+            if (nums.Length == 1)
+            {
+                if (nums[0] >= target) return 0; else return 1;
+            }
+
+            int index = nums.Length / 2;
+            int inc = index / 2;
+
+            while (true)
+            {
+                if (index >= nums.Length || index==0) return index;
+                if(nums[index] >= target && nums[index-1]<=target) return index;
+                if (nums[index] > target)
+                {
+                    index -= index>0? inc == 0 ? 1 : inc:0;
+                    inc /= 2;
+                }
+                else
+                {
+                    index += inc == 0 ? 1 : inc;
+                    inc /= 2;
+                }
+            }
+            
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace Solutions
     {
         static void Main(string[] args)
         {
-            int[] ar = new[] { 3,2,2,3 };
+            int[] ar = new[] { 1,3,5,6 };
+            int[] ar2 = new[] { 1 };
 
-            Console.WriteLine(_27_RemoveElement.RemoveElement(ar,3));
+            Console.WriteLine(_35_SearchInsertPosition.SearchInsert(ar2, 2)); //1
+            Console.WriteLine(_35_SearchInsertPosition.SearchInsert(ar,0)); 
+            Console.WriteLine(_35_SearchInsertPosition.SearchInsert(ar, 2));
+            Console.WriteLine(_35_SearchInsertPosition.SearchInsert(ar, 7));
             Console.ReadLine();
         }
     }
