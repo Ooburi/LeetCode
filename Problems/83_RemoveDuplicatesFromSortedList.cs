89fff66de4eea4de06f5aa3c1bf241b1fd050854
Pavel Andreevich <paandu.da@gmail.com>
2023-08-27 17:39:26 +0900

    81% Runtime, 39% Memory

---
+++ Problems/83_RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //83. Remove Duplicates from Sorted List
+
+    //Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+    //Return the linked list sorted as well.
+    internal class _83_RemoveDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            
+            ListNode temp = new(-101);
+            ListNode dummyHead = new(0,temp);
+
+            while (head != null)
+            {
+                if(head?.val == temp?.val)
+                {
+                    head = head.next;
+                    continue;
+                }
+                temp.next  = new(head?.val ?? 0);
+                temp = temp.next;
+            }
+            return dummyHead.next;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
