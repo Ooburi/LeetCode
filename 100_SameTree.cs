cd2458e60434139feff8da06364849a75b3c3713
Pavel Andreevich <paandu.da@gmail.com>
2023-08-28 15:34:33 +0900

    100. 80% Runtime. 89% Memory

---
+++ 100_SameTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    //100. Same Tree
+
+    //    Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+    //Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+    internal class _100_SameTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p?.val != q?.val || (q==null && p!=null) || (q!=null && p==null)) return false;
+            if (p == null && q == null) return true;
+
+            if(!IsSameTree(p.left, q.left)) return false;
+            if (!IsSameTree(p.right, q.right)) return false ;
+            return true;
+        }
+    }
+      
+}
