dc34093bf29f88552c766d3af8c87aacec45500e
Pavel Andreevich <paandu.da@gmail.com>
2023-08-28 18:30:44 +0900

    121. 96% Runtime, 60% Memory

---
+++ Problems/121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //121. Best Time to Buy and Sell Stock
+
+    //    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+    //You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future
+    //to sell that stock.
+
+    //Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
+    internal class _121_BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length < 2) return 0;
+            int profit = 0;
+            int min = prices[0];
+
+            for(int i = 0; i < prices.Length; i++)
+            {
+                if(min>prices[i]) min = prices[i];
+                if (prices[i] - min > profit) profit = prices[i] - min;
+            }
+            return profit;
+        }
+    }
+}
