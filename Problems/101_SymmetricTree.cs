d462855f6f25011c6ce250556adc9c9905b0a9cb
Pavel Andreevich <paandu.da@gmail.com>
2023-08-29 15:08:08 +0900

    101. 94% Runtime, 62% Memory WTF Solution

---
+++ Problems/101_SymmetricTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //101. Symmetric Tree
+    //Given the root of a binary tree, check whether it is a mirror of itself(i.e., symmetric around its center).
+    internal class _101_SymmetricTree
+    {
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if(root.left==null && root.right!=null || root.left!=null && root.left==null || root.left?.val != root.right?.val)
+                return false;
+
+            if (root.left == null && root.right == null) return true;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root.left);
+
+            StringBuilder sbLeftBranch = new StringBuilder("");
+            StringBuilder sbRightBranch = new StringBuilder("");
+
+
+            while(stack.Count > 0)
+            {
+                TreeNode node = stack.Pop();
+                sbLeftBranch.Append(node.val);
+
+                if(node.left!=null && node.right==null)
+                    sbLeftBranch.Append("L");
+                else if(node.left == null && node.right != null)
+                    sbLeftBranch.Append("R");
+
+                if (node.right != null) stack.Push(node.right);
+                if (node.left!=null) stack.Push(node.left);
+
+            }
+
+            stack.Push(root.right);
+
+            while (stack.Count > 0)
+            {
+                TreeNode node = stack.Pop();
+                sbRightBranch.Append(node.val);
+
+                if (node.left != null && node.right == null)
+                    sbRightBranch.Append("R");
+                else if (node.left == null && node.right != null)
+                    sbRightBranch.Append("L");
+                if (node.left != null) stack.Push(node.left);
+                if (node.right != null) stack.Push(node.right);
+
+            }
+
+            return String.Compare(sbLeftBranch.ToString(),sbRightBranch.ToString())==0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,25 +6,19 @@ namespace Solutions
     {
         static void Main(string[] args)
         {
-            int[] ar = new[] { 1,2,3,0,0,0 };
-            int[] ar2 = new[] { 2,5,6 };
+            TreeNode root = new(
+                1,
+                new(2, new(3), new(4)),
+                new(2, new(4), new(3))
+                );
 
-            int[] ar3 = new[] { 1 };
-            int[] ar4 = Array.Empty<int>();
+            TreeNode root2 = new(
+                1,
+                new(2, null, new(3)),
+                new(2, new(3), null)
+                );
 
-            _88_MergeTwoSortedList.Merge(ar, 3, ar2, 3);
-            _88_MergeTwoSortedList.Merge(ar3, 1, ar4, 0);
-
-            foreach (int i in ar)
-            {
-                Console.Write(i);
-            }
-            Console.WriteLine();
-            foreach (int i in ar3)
-            {
-                Console.Write(i);
-            }
-            Console.WriteLine();
+            Console.WriteLine(_101_SymmetricTree.IsSymmetric(root2));
             Console.ReadLine();
         }
     }
