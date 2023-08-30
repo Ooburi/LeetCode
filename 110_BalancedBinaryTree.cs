f95f34bfe3205b016b38018ff051152ef68d72f2
Pavel Andreevich <paandu.da@gmail.com>
2023-08-30 15:29:35 +0900

    110. 49% Runtime. 98% Memory

---
+++ 110_BalancedBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    //110. Balanced Binary Tree
+    //    Given a binary tree, determine if it is 
+    //height-balanced
+
+    internal class _110_BalancedBinaryTree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            return Height(root) >= 0;
+        }
+
+        public int Height(TreeNode node)
+        {
+
+            if (node == null)
+            {
+                return 0;
+            }
+            int leftSubtreeHeight = Height(node.left);
+            if (leftSubtreeHeight == -1) return -1;
+            int rightSubtreeHeight = Height(node.right);
+            if (rightSubtreeHeight == -1) return -1;
+            if (Math.Abs(leftSubtreeHeight - rightSubtreeHeight) > 1)
+            {
+                return -1;
+            }
+            return (Math.Max(leftSubtreeHeight, rightSubtreeHeight) + 1);
+        }
+    }
+}
