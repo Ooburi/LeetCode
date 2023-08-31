32799fb278997ed6ded28389db1b9ac8d9082a55
Pavel Andreevich <paandu.da@gmail.com>
2023-08-31 13:53:47 +0900

    125. 94% Runtime, 78% Memory

---
+++ 125_ValidPalindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    //125. Valid Palindrome
+    //A phrase is a palindrome if, after converting all uppercase letters into lowercase
+    //letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+    //Alphanumeric characters include letters and numbers.
+
+    //Given a string s, return true if it is a palindrome, or false otherwise.
+    internal class _125_ValidPalindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            int start = 0;
+            int end = s.Length-1;
+
+            while(start < end)
+            {
+                if (char.IsLetterOrDigit(s[start]) && char.IsLetterOrDigit(s[end]))
+                {
+                    if (char.ToLower(s[start]) != char.ToLower(s[end])) return false;
+                    else
+                    {
+                        start++;
+                        end--;
+                    }
+                }
+                else if (char.IsLetterOrDigit(s[start]))
+                {
+                    end--;
+                }
+                else start++;
+            }
+            return true;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace Solutions
         static void Main(string[] args)
         {
             var temp = _118_PascalsTriangle.GetRow(33);
-            Console.WriteLine();
+            string s = "A man, a plan, a canal: Panama";
+            //Console.WriteLine(char.IsNumber(' '));
+            Console.WriteLine(_125_ValidPalindrome.IsPalindrome(s));
             Console.ReadLine();
         }
     }
