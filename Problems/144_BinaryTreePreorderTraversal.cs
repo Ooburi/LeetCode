042c4688b0e0ac0bcfb4e9fce6f9b046f12aaba4
Pavel Andreevich <paandu.da@gmail.com>
2023-08-31 14:15:41 +0900

    45% Runtime, 94%Memory

---
+++ Problems/144_BinaryTreePreorderTraversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //144. Binary Tree Preorder Traversal
+    //Given the root of a binary tree, return the preorder traversal of its nodes' values.
+    internal class _144_BinaryTreePreorderTraversal
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> preorderTraversal = new List<int>();
+            if(root==null) return preorderTraversal;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                preorderTraversal.Add(node.val);
+                if(node.left!=null) stack.Push(node.left);
+                if (node.right != null) stack.Push(node.right);
+            }
+            return preorderTraversal;
+        }
+    }
+}
