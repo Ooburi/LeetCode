35e92634b3b87c2e8119a09e10783d759f0602cb
Pavel Andreevich <paandu.da@gmail.com>
2023-09-03 16:40:06 +0900

    70% Runtime. 41% Memory

---
+++ Problems/168_ExcelSheetColumTitle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //168. Excel Sheet Column Title
+    //Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+    internal class _168_ExcelSheetColumTitle
+    {
+        
+
+        public string ConvertToTitle(int columnNumber)
+        {
+            StringBuilder sb = new StringBuilder("");
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                char c = (char)('A' + columnNumber % 26);
+                columnNumber /= 26;
+                sb.Insert(0, c);
+            }
+            return sb.ToString();
+        }
+    }
+}
