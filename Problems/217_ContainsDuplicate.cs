32fcc4a7c42c690e7718a8ce3b425a192fd5ab7f
Pavel Andreevich <paandu.da@gmail.com>
2023-09-04 17:56:22 +0900

    56% Runtime, 50% Memory

---
+++ Problems/217_ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //217. Contains Duplicate
+
+    //Given an integer array nums, return true if any value appears at least twice
+    //in the array, and return false if every element is distinct.
+    internal class _217_ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Distinct<int>().Count() != nums.Length;
+        }
+    }
+}
