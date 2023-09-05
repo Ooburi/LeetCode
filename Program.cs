9a37e32c8d9c1660b9ea5ac26d9fb10f1a85761b
Pavel Andreevich <paandu.da@gmail.com>
2023-09-05 18:22:44 +0900

    138. 72%Runtime, 34%Memory

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Solutions.Problems;
+using static Solutions.Problems._138_CopyListWithRandomPointer;
 
 namespace Solutions
 {
@@ -6,8 +7,29 @@ namespace Solutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(_605_CanPlaceFlowers.CanPlaceFlowers(new int[] {1,0,0,0,1}, 2)); //false
-            Console.WriteLine(_605_CanPlaceFlowers.CanPlaceFlowers(new int[] { 1, 0, 0, 0, 0, 0, 1 }, 2)); //true
+            Node nodeOne = new(1);
+            Node nodeTwo = new(1);
+            Node nodeThree = new(1);
+            Node nodeFour = new(1);
+            Node nodeFive = new(1);
+
+            nodeOne.next = nodeTwo;
+            nodeOne.random = null;
+
+            nodeTwo.next = nodeThree;
+            nodeTwo.random = nodeOne;
+
+            nodeThree.next = nodeFour;
+            nodeThree.random = nodeFive;
+
+            nodeFour.next = nodeFive;
+            nodeFour.random = nodeThree;
+
+            nodeFive.next = null;
+            nodeFive.random = nodeOne;
+
+            _138_CopyListWithRandomPointer.CopyRandomList(nodeOne);
+            //Console.WriteLine(_605_CanPlaceFlowers.CanPlaceFlowers(new int[] { 1, 0, 0, 0, 0, 0, 1 }, 2)); //true
             Console.ReadLine();
         }
     }
