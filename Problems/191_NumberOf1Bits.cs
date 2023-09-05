ac267c0b1af22a1062cddb89d84a05f41b6fb7b9
Pavel Andreevich <paandu.da@gmail.com>
2023-09-05 18:35:32 +0900

    79%Runtime, 83%Memory

--- Problems/191_NumberOf1Bits.cs
+++ Problems/191_NumberOf1Bits.cs
@@ -13,13 +13,14 @@ namespace Solutions.Problems
     {
         public int HammingWeight(uint n)
         {
-            int count = 0;
-            for (int i = 16; i > 0; i--)
-            {
-                count += (int)n & 1;
-                n = n >> 1;
-            }
-            return count;
+            //int count = 0;
+            //for (int i = 16; i > 0; i--)
+            //{
+            //    count += (int)n & 1;
+            //    n = n >> 1;
+            //}
+            //return count;
+            return System.Numerics.BitOperations.PopCount(n);
         }
     }
 }
