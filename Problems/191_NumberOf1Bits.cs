fac15003404204dbb9a9142635da3df068b26c31
Pavel Andreevich <paandu.da@gmail.com>
2023-09-05 18:32:35 +0900

    73% Runtime, 41%Memory

---
+++ Problems/191_NumberOf1Bits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //191. Number of 1 Bits
+    //Write a function that takes the binary representation
+    //of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+    internal class _191_NumberOf1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            for (int i = 16; i > 0; i--)
+            {
+                count += (int)n & 1;
+                n = n >> 1;
+            }
+            return count;
+        }
+    }
+}
