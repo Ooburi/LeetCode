53af532f6cf96bce14402a0aa69d1542ee6d8de0
Pavel Andreevich <paandu.da@gmail.com>
2023-09-08 16:30:55 +0900

    202. 21% runtime, 15%memory

---
+++ Problems/202_HappyNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //202. Happy Number
+
+    //    Write an algorithm to determine if a number n is happy.
+    //    A happy number is a number defined by the following process:
+
+    //Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    //Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+    //Those numbers for which this process ends in 1 are happy.
+    //Return true if n is a happy number, and false if not.
+    internal class _202_HappyNumber
+    {
+        public static bool IsHappy(int n)
+        {
+            HashSet<int> sums = new HashSet<int>();
+
+            while (n!=1)
+            {
+                char[] chars = n.ToString().ToCharArray();
+                int sum = 0;
+                foreach(char c in chars)
+                {
+                    sum += (int)Math.Pow(Convert.ToInt32(c-48), 2);
+                }
+                n = sum;
+                if (sums.Contains(n)) return false;
+                sums.Add(n);
+            }
+            return true;
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,16 +7,8 @@ namespace Solutions
     {
         static void Main(string[] args)
         {
-            ListNode head = new(1);
-            head.next = new(2);
-            head.next.next = new(3);
-            head.next.next.next = new(4);
-            head.next.next.next.next = new(5);
-
             
-            //Console.WriteLine(_92_ReverseLinkedListII.ReverseBetween(head,2,4)); 
-            //Console.WriteLine(_92_ReverseLinkedListII.ReverseBetween(head.next.next.next.next, 1, 1));
-            Console.WriteLine(_92_ReverseLinkedListII.ReverseBetween(head.next.next.next, 1, 2));
+            Console.WriteLine(_202_HappyNumber.IsHappy(19));
             Console.ReadLine();
         }
     }
