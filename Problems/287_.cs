559de189f09763c1e2048358b96327bdbb8f3c65
Pavel Andreevich <paandu.da@gmail.com>
2023-09-19 21:28:16 +0900

    55% Runtime, 44% Memory

---
+++ Problems/287_.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Problems
+{
+    //287. Find the Duplicate Number
+    //Given an array of integers nums containing n + 1 integers where each integer is in the range[1, n] inclusive.
+
+    //There is only one repeated number in nums, return this repeated number.
+
+
+    //You must solve the problem without modifying the array nums and uses only constant extra space.
+    internal class _287_
+    {
+        public static int FindDuplicate(int[] nums)
+        {
+            int slow = nums[0];
+            int fast = nums[0];
+
+            do
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            } while (slow != fast);
+
+            slow = nums[0];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+
+            return slow;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,10 @@ namespace Solutions
         static void Main(string[] args)
         {
             
-            Console.WriteLine(_1337_.KWeakestRows(new int[4][]
-            {
-                new int[] { 1,1,1,0,0,0,0,0,0,0,0,0,0,0,0 }, //3
-                new int[] { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },//12
-                new int[] { 0,0,0,0,0,0,0,0,0,0,0 },//0
-                new int[] { 1,1,1,0,0,0 }//3
-            },0
-                ));
-            Console.WriteLine(_1337_.KWeakestRows(new int[0][]
-            {
-            }, 0
-                ));
+            
+            Console.WriteLine(_287_.FindDuplicate(new int[5]
+            { 1, 2,3,4,2
+            }));
             Console.ReadLine();
         }
     }
