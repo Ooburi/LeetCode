c4099f674095ceb2b9174bfe23857f83856be0b1
Pavel Andreevich <paandu.da@gmail.com>
2023-10-04 17:59:35 +0900

    69% Runtime, 63% Memory

--- Problems/706_.cs
+++ Problems/706_.cs
@@ -17,93 +17,24 @@ namespace Solutions.Problems
     //void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
     internal class _706_
     {
-        public class ListNode
-        {
-            public int Key { get; set; }
-            public int Value { get; set; }
-            public ListNode Next { get; set; }
-
-            public ListNode(int key = -1, int value = -1, ListNode next = null)
-            {
-                Key = key;
-                Value = value;
-                Next = next;
-            }
-        }
-        public class MyHashMap
+        int?[] arr = new int?[1000001];
+        public MyHashMap()
         {
 
-            private ListNode[] map;
-            public MyHashMap()
-            {
-                map = new ListNode[1000];
-            }
-
-            private int Hash(int key)
-            {
-                return key % map.Length;
-            }
-
-            public void Put(int key, int value)
-            {
-                int index = Hash(key);
-                ListNode current = map[index];
-
-                while (current != null)
-                {
-                    if (current.Key == key)
-                    {
-                        current.Value = value;
-                        return;
-                    }
-                    current = current.Next;
-                }
-
-                map[index] = new ListNode(key, value, map[index]);
-            }
-
-            public int Get(int key)
-            {
-                int index = Hash(key);
-                ListNode current = map[index];
-
-                while (current != null)
-                {
-                    if (current.Key == key)
-                    {
-                        return current.Value;
-                    }
-                    current = current.Next;
-                }
-
-                return -1;
-            }
-
-            public void Remove(int key)
-            {
-                int index = Hash(key);
-                ListNode current = map[index];
-                ListNode prev = null;
-
-                while (current != null)
-                {
-                    if (current.Key == key)
-                    {
-                        if (prev != null)
-                        {
-                            prev.Next = current.Next;
-                        }
-                        else
-                        {
-                            map[index] = current.Next;
-                        }
-                        return;
-                    }
-                    prev = current;
-                    current = current.Next;
-                }
-            }
+        }
+        public void Put(int key, int value)
+        {
+            arr[key] = value;
         }
 
+        public int Get(int key)
+        {
+            return arr[key] == null ? -1 : (int)arr[key];
+        }
+
+        public void Remove(int key)
+        {
+            arr[key] = null;
+        }
     }
 }
